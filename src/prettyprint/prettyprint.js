e049ea022b0a5525f94c8b3e8b54efb54fcfe135
Philipp Burgmer <burgmer@weiglewilczek.com>
2014-05-15 14:52:56 +0200

    feat(prettyprint): improve pretty-print directive to work with unescaped html
    BREAKING CHANGE: w11k-pretty-print directive now has to be placed at the element containing the code
    
    Example:
    <pre w11k-pretty-print lang="html">
    <div class="screen">
      &lt;div w11k-slides&gt;&lt;/div&gt;
    </div>
    </pre>

--- src/prettyprint/prettyprint.js
+++ src/prettyprint/prettyprint.js
@@ -1,11 +1,30 @@
 'use strict';
 
-angular.module('w11k.slides').directive('w11kPrettyPrint', ['$window', function ($window) {
+angular.module('w11k.slides').directive('w11kPrettyPrint', ['$window', '$document', function ($window, $document) {
+
+  var escapeHTML = function (html) {
+    if (angular.isUndefined(html)) {
+      return;
+    }
+
+    return html.replace(/>/g, '&gt;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+  };
+
   return {
     restrict: 'A',
-    link: function (scope, element) {
-      if (angular.isFunction($window.prettyPrint)) {
-        $window.prettyPrint(angular.noop, element[0]);
+    compile: function (tElement, tAttrs) {
+      if (angular.isFunction($window.prettyPrintOne)) {
+        var html = tElement.html();
+        var escapedHtml = escapeHTML(html);
+        var prettifiedHtml = $window.prettyPrintOne(escapedHtml, tAttrs.lang, true);
+
+        var preElement = $document[0].createElement('pre');
+        preElement.classList.add('prettyprint');
+        preElement.classList.add('linenums');
+
+        preElement.innerHTML = prettifiedHtml;
+
+        tElement.replaceWith(preElement);
       }
     }
   };
